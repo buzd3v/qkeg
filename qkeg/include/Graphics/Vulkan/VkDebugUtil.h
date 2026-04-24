d3be7cfad30618ecd547839c7f8a77e196e47fb1
neik <kanashkaze02@gmail.com>
2026-04-24 21:05:42 +0700

    feat(build tool): migrate submodules to cpm.cmake

--- qkeg/include/Graphics/Vulkan/VkDebugUtil.h
+++ qkeg/include/Graphics/Vulkan/VkDebugUtil.h
@@ -8,13 +8,13 @@
 namespace VkDbgUtil
 {
 
-void cmdBeginLabel(VkCommandBuffer cmd, const char *label, glm::vec4 color = glm::vec4{1.f});
-void cmdEndLabel(VkCommandBuffer cmd);
-void addDebugLabel(VkDevice device, VkImage image, const char *label);
-void addDebugLabel(VkDevice device, VkImageView imageView, const char *label);
-void addDebugLabel(VkDevice device, VkShaderModule shaderModule, const char *label);
-void addDebugLabel(VkDevice device, VkPipeline pipeline, const char *label);
-void addDebugLabel(VkDevice device, VkPipelineLayout layout, const char *label);
-void addDebugLabel(VkDevice device, VkBuffer buffer, const char *label);
-void addDebugLabel(VkDevice device, VkSampler sampler, const char *label);
+// void cmdBeginLabel(VkCommandBuffer cmd, const char *label, glm::vec4 color = glm::vec4{1.f});
+// void cmdEndLabel(VkCommandBuffer cmd);
+// void addDebugLabel(VkDevice device, VkImage image, const char *label);
+// void addDebugLabel(VkDevice device, VkImageView imageView, const char *label);
+// void addDebugLabel(VkDevice device, VkShaderModule shaderModule, const char *label);
+// void addDebugLabel(VkDevice device, VkPipeline pipeline, const char *label);
+// void addDebugLabel(VkDevice device, VkPipelineLayout layout, const char *label);
+// void addDebugLabel(VkDevice device, VkBuffer buffer, const char *label);
+// void addDebugLabel(VkDevice device, VkSampler sampler, const char *label);
 }; // namespace VkDbgUtil
